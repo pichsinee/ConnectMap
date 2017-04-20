b5bcdd95cc112f4292b837a6d58f08555f40bf01
pichsinee <p.pichsinee@gmail.com>
2017-04-20 10:06:07 +0700

    Design UI

---
+++ app/src/main/java/sdu/alice/connectmap/CallMap.java
@@ -0,0 +1,15 @@
+package sdu.alice.connectmap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_map);
+    }   //onCreate Method
+
+
+}   //Main Class
